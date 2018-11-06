4bd71d91dd45ff9e432e73168333c8db7192def4
MaximKuklin <43599840+MaximKuklin@users.noreply.github.com>
2018-11-06 20:39:27 +0300

    Add files via upload

---
+++ src/main3.cpp
@@ -0,0 +1,15 @@
+#icnlude "task3.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const size_t n = 5;
+	char arr[n]{ '1', '2', '3', '4', '5' };
+	map<char, n>(arr, change);
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << ' ';
+
+	return 0;
+}
