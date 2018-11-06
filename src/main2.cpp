e8a61b961370ab8edfc43d57c1cd64e61e16cef6
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2018-11-06 22:45:43 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -4,10 +4,5 @@ using namespace std;
 
 int main()
 {
-	const size_t n = 5;
-	char* arr;
-	arr = createArr<char, n>(gen);
-	for (size_t i = 0; i < n; i++) {
-		cout << arr[i] << "|";
-	}
+	return 0
 }
