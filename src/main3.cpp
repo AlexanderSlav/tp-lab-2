267d0c8631908849da3129dd66d1a03c04464ac1
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2018-11-06 23:14:17 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include "task3.h"
+using namespace std;
+
+template<class T>
+	T change(T val)
+	{
+		return val + 1;
+	}
+
+	template<>
+	char* change(char* val)
+	{
+		size_t n = strlen(val);
+		for(size_t i = 0; i < n; i++)
+			val[i] = val[i] + 1;
+		return val;
+	}
 
 int main()
 {
-	return 0;
+	const size_t n = 5;
+	char arr[n]{ '1', '2', '3', '4', '5' };
+	map<char, n>(arr, change);
+	for (size_t i = 0; i < n; i++) {
+		cout << arr[i] << "|";
+	}
 }
