22c956ea487c52a48033a2cdf0e9e64d15f8f96d
Chernozemova <44014905+Chernozemova@users.noreply.github.com>
2018-11-10 18:11:17 +0300

    Add files via upload

---
+++ src/main2.cpp
@@ -0,0 +1,14 @@
+#include "task2.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const size_t n = 5;
+	int* arr;
+	arr = createArr<int, n>(gen);
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] <<' ';
+}
