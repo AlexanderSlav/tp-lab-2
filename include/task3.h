30bf0426613dbc5dd62d7a9750be99b4ea8858ed
Chernozemova <44014905+Chernozemova@users.noreply.github.com>
2018-11-10 18:24:20 +0300

    Update task3.h

--- include/task3.h
+++ include/task3.h
@@ -3,21 +3,6 @@
 
 using namespace std;
 
-template<class T>
-T change(T val)
-{
-	return val + 1;
-}
-
-template<>
-char* change(char* val)
-{
-	size_t n = strlen(val);
-	for (size_t i = 0; i < n; i++)
-		val[i] = val[i] + 1;
-	return val;
-}
-
 template <typename T, size_t N>
 T *map(T(&mass)[N], T(*change)(T))
 {
