5aef45259d77f6f436b7c1e08271203f6979fb69
rayonnant14 <smolpolina199906@icloud.com>
2018-11-13 21:07:06 +0300

    all tests

---
+++ src/main2.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const size_t n = 5;
+	int* ptrI;
+	char** ptrC;
+	ptrI = createArr<int, n>(gen);
+	for (int i = 0; i < n; i++) cout << *(ptrI + i) << ' ';
+	cout << '\n';
+	ptrC = createArr<char*, n>(gen);
+	for (int i = 0; i < n; i++) cout << *(ptrC+i) << ' ';
+	system("pause");
+	return 0;
+}
