05c5dc8b05aa7d7592a029ea070fe05005f9ae12
Vailina <43599573+Vailina@users.noreply.github.com>
2018-11-20 11:27:22 +0300

    4

--- src/main3.cpp
+++ src/main3.cpp
@@ -3,6 +3,21 @@
 #include <iostream>
 
 using namespace std;
+template<class T>
+T change(T val)
+{
+	return val + 1;
+}
+
+template<>
+char* change(char* val)
+{
+	size_t n = strlen(val);
+	for (int i = 0; i < n; i++)
+		val[i] = val[i] + 1;
+	return val;
+}
+
 
 int main()
 {
