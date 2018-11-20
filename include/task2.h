2d7e6c4e2eeb241ef1025c72113bdb0396a97be5
Vailina <43599573+Vailina@users.noreply.github.com>
2018-11-20 11:24:03 +0300

    1

--- include/task2.h
+++ include/task2.h
@@ -1,23 +1,6 @@
 
 #pragma once
 
-template<class T>
-T gen(){
-	static int t = 48;
-	return t++;
-}
-
-	template<>
-char* gen(){
-	static unsigned t = 0;
-	t++;
-	char* s = new char[t + 1];
-
-	for (int i = 0; i < t; i++)
-		s[i] = '0' + i;
-	s[t] = '\0';
-	return s;
-	}
 template <class T, size_t n>
 T * createArr(T(*gen)()) {
 	T * mas = new T[n];
