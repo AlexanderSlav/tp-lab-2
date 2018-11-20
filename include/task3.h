07c24b858b4775c6157ac9f0258629181e17c179
Vailina <43599573+Vailina@users.noreply.github.com>
2018-11-20 11:26:46 +0300

    3

--- include/task3.h
+++ include/task3.h
@@ -1,21 +1,6 @@
 
 #pragma once
 
-template<class T>
-T change(T val)
-{
-	return val + 1;
-}
-
-template<>
-char* change(char* val)
-{
-	size_t n = strlen(val);
-	for (int i = 0; i < n; i++)
-		val[i] = val[i] + 1;
-	return val;
-}
-
 template<class T,int n>
 void map(T (&mas)[n], T(*change)(T)){
 	for (int i = 0; i < n;i++)
