6b314318631519954440fde0187467c25ecae04f
Vailina <43599573+Vailina@users.noreply.github.com>
2018-11-20 11:24:57 +0300

    2

--- src/main2.cpp
+++ src/main2.cpp
@@ -3,7 +3,23 @@
 #include <iostream>
 
 using namespace std;
+template<class T>
+T gen(){
+	static int t = 48;
+	return t++;
+}
 
+template<>
+char* gen(){
+	static unsigned t = 0;
+	t++;
+	char* s = new char[t + 1];
+
+	for (int i = 0; i < t; i++)
+		s[i] = '0' + i;
+	s[t] = '\0';
+	return s;
+}
 int main()
 {
 		const size_t n = 5;
