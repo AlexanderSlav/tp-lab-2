f8c6fc058fd0e730805db2e3cd1d395f6243996a
Vailina <43599573+Vailina@users.noreply.github.com>
2018-11-20 11:32:19 +0300

    include <cstring>

--- include/task3.h
+++ include/task3.h
@@ -1,7 +1,7 @@
-
 #pragma once
+#include <cstring>
 
-template<class T,int n>
+template<class T,size_t n>
 void map(T (&mas)[n], T(*change)(T)){
 	for (int i = 0; i < n;i++)
 		mas[i] = change(mas[i]);
