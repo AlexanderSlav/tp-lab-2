a38ac0fa29183e44ccf4bdf6e6dfa0826bed1a91
dlots <dlots42@gmail.com>
2019-02-13 02:49:05 +0300

    lab2Sazanov

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,4 +1,4 @@
-#include "task3.h"
+#include "../include/task3.h"
 
 template <typename T>
 T change(T elem){
@@ -6,11 +6,23 @@ T change(T elem){
   //cout << "ok";
 }
 
+template <>
+char* change(char* elem){
+  elem="aa";
+  return elem;
+}
+
 int main(){
   int arr[5]={1,2,3,4,5};
+  char* arr1[5] = { (char*)"7aaaaaaa", (char*)"6aaaaaa", (char*)"5aaaaa" ,(char*)"4aaaa" ,(char*)"3aaa" };
   for (int i =0; i < 5; i++) cout <<arr[i] << " ";
   cout << "\n";
+  for (int i =0; i < 5; i++) cout <<arr1[i] << " ";
+  cout << "\n";
   map(arr, change, 5);
+  map(arr1, change, 5);
   for (int i =0; i < 5; i++) cout <<arr[i] << " ";
+  cout << "\n";
+  for (int i =0; i < 5; i++) cout <<arr1[i] << " ";
   return 0;
 }
