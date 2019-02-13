71aec1d16df202c3e0c52869c9a5ac1f937bb547
dlots <dlots42@gmail.com>
2019-02-13 08:59:41 +0300

    lab2Sazanov

--- include/task2.h
+++ include/task2.h
@@ -7,7 +7,7 @@ using namespace std;
 template <typename T, unsigned int size>
 T* createArr(T (*generator)()) {
   T* newArr = new T[size];
-  T key = 2;
+  //T key = 2;
   for (unsigned int i=0; i < size; i++) {
     newArr[i]=generator(/*key*/);
   }
--- src/main2.cpp
+++ src/main2.cpp
@@ -8,6 +8,11 @@ T gen(/*T key*/) {
   //cout<< key*2;
 }
 
+template <>
+char* gen() {
+  char* elem = (char*)"elem";
+}
+
 /*template <>
 char* gen(/*const char* key*comm/) {
   //cout << "specific";
@@ -24,9 +29,14 @@ int main()
   //float floatkey=1.3;
   //int intkey=2;
   int *testarr = createArr<int, size>(gen);
+  char* *testarr1 = createArr<char*, size>(gen);
   //const char* charkey="fuf";
   for (unsigned int i = 0; i < size; i++) {
     cout << testarr[i] << " ";
+  }
+  cout << "\n";
+  for (unsigned int i = 0; i < size; i++) {
+    cout << testarr1[i] << " ";
   }
   return 0;
 }
