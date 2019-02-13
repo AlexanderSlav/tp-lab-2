7c0afd1c9aed0c08c2be240a310110e3a0a804a5
dlots <dlots42@gmail.com>
2019-02-13 08:51:39 +0300

    lab2Sazanov

--- include/task3.h
+++ include/task3.h
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-void map(T array[], T (*changer)(T), unsigned int size){
+template <typename T, const unsigned int size>
+void map(T array[], T (*changer)(T)){
   for (unsigned int i = 0; i < size; i++) {
     array[i] = changer(array[i]);
   }
--- src/main3.cpp
+++ src/main3.cpp
@@ -8,19 +8,20 @@ T change(T elem){
 
 template <>
 char* change(char* elem){
-  elem="aa";
+  elem=(char*)"aa";
   return elem;
 }
 
 int main(){
+  const unsigned int size = 5;
   int arr[5]={1,2,3,4,5};
   char* arr1[5] = { (char*)"7aaaaaaa", (char*)"6aaaaaa", (char*)"5aaaaa" ,(char*)"4aaaa" ,(char*)"3aaa" };
   for (int i =0; i < 5; i++) cout <<arr[i] << " ";
   cout << "\n";
   for (int i =0; i < 5; i++) cout <<arr1[i] << " ";
   cout << "\n";
-  map(arr, change, 5);
-  map(arr1, change, 5);
+  map<int, size>(arr, change);
+  map<char*, size>(arr1, change);
   for (int i =0; i < 5; i++) cout <<arr[i] << " ";
   cout << "\n";
   for (int i =0; i < 5; i++) cout <<arr1[i] << " ";
