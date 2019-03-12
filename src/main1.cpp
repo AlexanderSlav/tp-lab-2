f947a505c4b68cabaf0f89e1e5ebfe43d0ee90c5
AlexanderSlav <alexander.slavutin@gmail.com>
2019-03-12 23:27:56 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -4,7 +4,7 @@
 #include "task1.h"
 
 int main(){
-    const size_t size = 7 ;
+    const size_t size = 8 ;
     int arr[size] = {41,221,607,156,89,56,78,98};
     msort(arr, size);
     for(auto i = 0; i < size ; i++ ){
