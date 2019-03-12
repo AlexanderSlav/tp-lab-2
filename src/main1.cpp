59c49aaf014e2ce090ebbf67815b6b2f6f716b32
AlexanderSlav <alexander.slavutin@gmail.com>
2019-03-12 23:27:41 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -5,7 +5,7 @@
 
 int main(){
     const size_t size = 7 ;
-    int arr[size] = {41,25,67,89,56,78,98};
+    int arr[size] = {41,221,607,156,89,56,78,98};
     msort(arr, size);
     for(auto i = 0; i < size ; i++ ){
        std::cout << arr[i] << " ";
