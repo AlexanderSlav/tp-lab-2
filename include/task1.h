64e620dd82515a1ff0b0f442b92be71c32f12b1c
AlexSlav <alexander.slavutin@rambler.ru>
2019-03-13 18:46:58 +0300

    1 task completed

--- include/task1.h
+++ include/task1.h
@@ -3,8 +3,6 @@
 //
 #pragma once
 #include <iostream>
-#include <functional>
-#include <memory>
 #include <cstring>
 
 template <typename T>
@@ -21,24 +19,26 @@ bool compare(char* a, char* b)
 template <typename T>
 void msort(T* array, size_t size)
 {
-    if (size < 2) // Stop the recursion
-        return;
+    if (size == 1)
+       return;
+
 
     size_t const leftsize = size / 2;
     size_t const rightsize = size - leftsize;
 
-    msort(&array[0], leftsize); // Sort the right half
-    msort(&array[leftsize], rightsize); // Sort the left half
+    msort(&array[0], leftsize); // Sort the left half
+    msort(&array[leftsize], rightsize); // Sort the right half
 
-    T* tmp = new T[size]; // Result array
+    T* tmp = new T[size]; // Temporary array for result
 
-    size_t lidx(0), ridx(leftsize), idx(0); // Index variables for left half, right half and temporary array
+    size_t lidx(0), ridx(leftsize), idx(0); // lidx - for the left half, ridx - for the right half,
+    // idx - for the tmp array
 
-    while ((lidx < leftsize) || (ridx < size)) // Iterate over the whole array
+    while ((lidx < leftsize) || (ridx < size))
     {
-        if ((lidx < leftsize) && (ridx < size)) // Check if there still are number in both halves
+        if ((lidx < leftsize) && (ridx < size))
         {
-            if (compare(array[lidx], array[ridx])) // The value from the left half is bigger
+            if (compare(array[lidx], array[ridx]))
             {
                 tmp[idx++] = array[lidx];
                 lidx++;
@@ -51,7 +51,7 @@ void msort(T* array, size_t size)
             }
         }
 
-        else if (lidx < size / 2) // Values remain only in the left half
+        else if (lidx < leftsize)
         {
             tmp[idx++] = array[lidx];
             lidx++;
@@ -64,8 +64,18 @@ void msort(T* array, size_t size)
         }
     }
 
-    for (size_t i(0); i < size; i++)
-        array[i] = tmp[i];
+    for(auto i(0); i < size ; i++ ){
+        std::cout << array[i] << " ";
+    }
+    std::cout<< "|"; // before sort for one iteration (just for clear)
+
+
+    std::copy(tmp, &tmp[size], array);
+
+    for(auto i(0); i < size ; i++ ){
+        std::cout << array[i] << " ";
+    }
+    std::cout<< "|"; // after sort
 
     delete[] tmp;
 }
--- src/main1.cpp
+++ src/main1.cpp
@@ -7,6 +7,7 @@ int main(){
     const size_t size = 7 ;
     int arr[size] = {41,25,67,89,56,78,98};
     msort(arr, size);
+    std::cout<< "|| ";
     for(auto i = 0; i < size ; i++ ){
        std::cout << arr[i] << " ";
     }
